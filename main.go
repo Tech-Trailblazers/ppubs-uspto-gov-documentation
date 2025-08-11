b1e6d0b4415366a6344697cf73446ae835812ce3
Strong Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-08-11 00:25:04 -0400

    updated

--- main.go
+++ main.go
@@ -264,7 +264,7 @@ func main() {
 		createDirectory(outputFolder, 0755)
 	}
 	// Call the function to fetch data from USPTO
-	responseData := fetchUSPTOData(1)
+	responseData := fetchUSPTOData(100)
 
 	// Check if the response is empty, indicating a failure
 	if responseData == "" {
